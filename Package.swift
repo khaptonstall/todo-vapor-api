f73eb25fd0d106192a9883789330bd4decd838eb
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2019-11-19 14:13:11 -0600

    added user and token models

--- Package.swift
+++ Package.swift
@@ -7,14 +7,12 @@ let package = Package(
         .library(name: "todo-api", targets: ["App"]),
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
---
+++ Sources/App/Models/Token.swift
@@ -0,0 +1,72 @@
+import Authentication
+import FluentPostgreSQL
+import Vapor
+
+final class Token: Codable {
+    
+    // MARK: Properties
+    
+    var id: UUID?
+    var token: String
+    var userID: User.ID
+    
+    // MARK: Initialization
+    
+    init(token: String, userID: User.ID) {
+        self.token = token
+        self.userID = userID
+    }
+    
+}
+
+// MARK: - PostgreSQLUUIDModel
+
+extension Token: PostgreSQLUUIDModel {}
+
+// MARK: - Content
+
+extension Token: Content {}
+
+// MARK: - Migration
+
+extension Token: Migration {
+    
+    static func prepare(on connection: PostgreSQLConnection) -> EventLoopFuture<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            
+            // Create a foreign key constraint with a User
+            builder.reference(from: \.userID, to: \User.id)
+        }
+    }
+    
+}
+
+// MARK: Authentication.Token
+
+extension Token: Authentication.Token {
+    
+    static let userIDKey: UserIDKey = \Token.userID
+    
+    typealias UserType = User
+    
+}
+
+// MARK: - BearerAuthenticatable
+
+extension Token: BearerAuthenticatable {
+    
+    static let tokenKey: TokenKey = \Token.token
+    
+}
+
+// MARK: Utilities
+
+extension Token {
+    
+    static func generate(for user: User) throws -> Token {
+        let random = try CryptoRandom().generateData(count: 16)
+        return try Token(token: random.base64EncodedString(), userID: user.requireID())
+    }
+    
+}
